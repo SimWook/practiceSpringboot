9a7884e22e2abe9bf56f6b2a7ea56cac0c69ee75
シム ウク <astralkaiser89@gmail.com>
2023-11-19 20:28:33 +0900

    bean check

---
+++ src/test/java/com/practice/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,40 @@
+package com.practice.core.beanfind;
+
+import com.practice.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("すべてのBean出力")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+        }
+
+    }
+
+    @Test
+    @DisplayName("アプリケーションのBean出力")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //Role ROLE_APPLICATION: 直接登録したアプリケーションbean
+            //Role ROLE_INFRASTRUCTURE: Springが内部で使うbean
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+
+    }
+}
