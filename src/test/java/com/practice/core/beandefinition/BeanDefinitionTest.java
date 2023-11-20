d10ebf3fb41544e06b36f8ec8ef8196b2be78149
シム ウク <astralkaiser89@gmail.com>
2023-11-20 21:11:50 +0900

    bean practice

---
+++ src/test/java/com/practice/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,26 @@
+package com.practice.core.beandefinition;
+
+import com.practice.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    //GenericApplicationContext ac = new GenericXmlApplicationContext("appconfig.xml");
+
+    @Test
+    @DisplayName("Bean設定メター情報確認")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+}
